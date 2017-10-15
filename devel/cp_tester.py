06e9b69f4d207e0606a0b01b04e6e2086faf65b2
Kohei Sakamoto <y2005r@gmail.com>
2017-10-15 16:23:19 +0900

    [chg]change data format from Json to plane

--- devel/cp_tester.py
+++ devel/cp_tester.py
@@ -4,23 +4,20 @@ Usage: python cp_tester.py <test subject> <test setting>
 Requirement:
   Test subject: Command line executions that take test data as standard input
                 and return results as standard output.
-  Test setting: JSON files for test data and correct result for the data.
-                For dry run, output shoud not be defined.
-                JSON scheme is following.
-                The inputs and outputs are defined as multiple lines of
-                standatd input/output.
-                Last newlines are not submitted/expected for inputs/outputs.
-                  {
-                    data: [
-                      input:  [ {string} ],
-                      output: [ {string} ] 
-                    ]
-                  }
-  
+  Test setting: A formatted file for test data. The format is followings.
+                As showed in followings, multiple input/output pairs are allowed.
+                INPUT
+                <test input data 1 for multiple lines>
+                OUTPUT
+                <test output data 1 for multiple lines>
+                INPUT
+                <test input data 2 for multiple lines>
+                OUTPUT
+                <test output data 2 for multiple lines>
+ 
 """
 
 import sys
-import json
 import time
 import math
 from subprocess import Popen, PIPE, STDOUT
@@ -33,12 +30,48 @@ def usage():
     """
     print("usage : {0} <program> <test setting>".format(argv[0]))
 
+def read_test_file(filename):
+    test_data    = None
+    current_data = None
+    init         = False
+    for line in open(filename, 'r'):
+        if line.rstrip() == "INPUT":
+            if not init:
+                # first test data
+                test_data    = []
+                current_data = {}
+                init = True
+            else:
+                # new test data
+                if ("input" not in current_data) or ("output" not in current_data):
+                    raise ValueError("INPUT or OUTPUT not exists.")
+                test_data.append(current_data);
+                current_data = {}
+            current_data["input"] = ""
+        elif line.rstrip() == "OUTPUT":
+            if not init:
+                raise ValueError("NO INPUT")
+            if "output" in current_data:
+                raise ValueError("OUTPUT duplicated")
+            current_data["output"] = ""
+        else:
+            if not init:
+                raise ValueError("NO INPUT")
+            if not "input" in current_data:
+                raise ValueError("NO INPUT")
+
+            if not "output" in current_data:
+                current_data["input"] += line
+            else:
+                current_data["output"] += line
+    return test_data
+
 def test(cmd, test_case):
     """test cmd with one test_case
       cmd:       name of command for test
       test_case: one test case setting
     """
-    input = "\n".join(test_case["input"])
+    input = test_case["input"]
 
     p = Popen([cmd], stdout=PIPE, stdin=PIPE, stderr=STDOUT)
     start  = time.time()
@@ -57,8 +90,8 @@ def test(cmd, test_case):
     if "output" not in test_case:
         return;
 
-    expected = "\n".join(test_case["output"])
-    if output == expected:
+    expected = test_case["output"]
+    if output.rstrip() == expected.rstrip():
         print("OK")
     else:
         print("NG : expected")
@@ -69,9 +102,9 @@ if (argc < 3):
     usage()
     exit(-1)
 
-test_file = open(argv[2])
-test_data = json.load(test_file)
+test_file = argv[2]
+test_data = read_test_file(test_file)
 
-for i, test_case in enumerate(test_data["data"]):
+for i, test_case in enumerate(test_data):
     print("test case {0}...".format(i))
     test(argv[1], test_case)
